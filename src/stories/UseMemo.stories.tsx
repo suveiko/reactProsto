5b7d523b3f4ca809f07bb6ed829a80cc0e4d0a3c
suveiko <suveikosasha@gmail.com>
2022-06-13 14:59:27 +0400

    Created file useMemo and component DifficultCountingExample

---
+++ src/stories/UseMemo.stories.tsx
@@ -0,0 +1,42 @@
+import {useMemo, useState} from "react";
+
+export default {
+    title: 'useMemo'
+}
+
+
+export const DifficultCountingExample = () => {
+
+    const [a, setA] = useState(5)
+    const [b, setB] = useState(5)
+
+    let resultA;
+    let resultB = 1
+
+    resultA = useMemo(() => {
+        let tempResultA = 1
+        for (let i = 1; i <= a; i++) {
+            let fake = 0
+            while (fake < 100000000) {
+                fake++
+                const fakeValue = Math.random()
+            }
+            tempResultA *= i
+
+        }
+        return tempResultA
+    }, [a])
+
+
+    for (let i = 1; i <= b; i++) {
+        resultB *= i
+    }
+
+    return <>
+        <input value={a} onChange={(e) => setA(+e.currentTarget.value)}/>
+        <input value={b} onChange={(e) => setB(+e.currentTarget.value)}/>
+        <hr/>
+        <div>Result for a: {resultA}</div>
+        <div>Result for b: {resultB}</div>
+    </>
+}
