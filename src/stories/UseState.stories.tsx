425d447f1fdda37fc715fb3a7062ab825fdafdcd
suveiko <suveikosasha@gmail.com>
2022-07-01 15:31:41 +0400

    feat: create UseState.stories

---
+++ src/stories/UseState.stories.tsx
@@ -0,0 +1,23 @@
+import {useState} from "react";
+
+
+export default {
+    title: 'useState demo'
+}
+
+
+function generateData() {
+    console.log('generateData')
+    return 1
+}
+
+export const Example1 = () => {
+    console.log('Example1')
+
+    const [counter, setCounter] = useState(generateData)
+
+    return <>
+        <button onClick={() => setCounter(state => state + 1)}>increase</button>
+        {counter}
+    </>
+}
