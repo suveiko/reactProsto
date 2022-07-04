f5f51112c0355e88f872b344c28be0c1c137783a
suveiko <suveikosasha@gmail.com>
2022-07-04 16:40:51 +0400

    feat: create Clock.stories

---
+++ src/stories/Clock.stories.tsx
@@ -0,0 +1,108 @@
+import React, {Dispatch, SetStateAction, useEffect, useState} from 'react';
+
+import s from './Clock.module.css'
+
+export default {
+    title: 'Clock demo'
+}
+
+
+export const Clock = () => {
+
+    const [date, setDate] = useState(new Date())
+    const [change, setChange] = useState(true)
+
+    useEffect(() => {
+
+        const intervalId = setInterval(() => {
+            setDate(new Date())
+        }, 1000)
+
+        return () => clearInterval(intervalId)
+
+    }, [])
+
+    const getTwoDigitsString = (num: number) => num < 10 ? `0${num}` : num
+
+    return (
+        <>
+            <button
+                onClick={() => setChange(!change)}
+                style={{cursor:'pointer'}}
+            >
+                Change clock
+            </button>
+            {
+                change ?
+                    <div style={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        fontSize: '40px'
+                    }}>
+                        <span>{getTwoDigitsString(date.getHours())}</span>
+                        :
+                        <span>{getTwoDigitsString(date.getMinutes())}</span>
+                        :
+                        <span>{getTwoDigitsString(date.getSeconds())}</span>
+                    </div>
+                    : <AnalogClock date={date} setDate={setDate}/>
+            }
+        </>
+    );
+};
+
+
+type AnalogClockType = {
+    date: Date
+    setDate: Dispatch<SetStateAction<Date>>
+}
+
+export const AnalogClock = ({date, setDate}: AnalogClockType) => {
+
+    const classNameForClock = [
+        {number: 12, className: s.twelve},
+        {number: 1, className: s.one},
+        {number: 2, className: s.two},
+        {number: 3, className: s.three},
+        {number: 4, className: s.four},
+        {number: 5, className: s.five},
+        {number: 6, className: s.six},
+        {number: 7, className: s.seven},
+        {number: 8, className: s.eight},
+        {number: 9, className: s.nine},
+        {number: 10, className: s.ten},
+        {number: 11, className: s.eleven},
+    ]
+
+    useEffect(() => {
+
+        const intervalId = setInterval(() => {
+            setDate(new Date())
+        }, 1000)
+
+        return () => clearInterval(intervalId)
+
+    }, [setDate])
+
+
+    return (
+        <div className={s.clock}>
+            <div
+                className={s.hour_hand}
+                style={{transform: `rotateZ(${date.getHours() * 30}deg)`}}
+            />
+            <div
+                className={s.min_hand}
+                style={{transform: `rotateZ(${date.getMinutes() * 6}deg)`}}
+            />
+            <div
+                className={s.sec_hand}
+                style={{transform: `rotateZ(${date.getSeconds() * 6}deg)`}}
+            />
+            {
+                classNameForClock.map(({className, number}) =>
+                    <span className={className}>{number}</span>
+                )}
+        </div>
+    );
+}
